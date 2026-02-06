c367e5c4fd3ce0e95853dbc11c2b84e263f21349
Web3WizardZ <noblen.94@gmail.com>
2026-02-06 16:06:48 +0200

    modified:   src/app/globals.css 	deleted:    src/types/panna-sdk.d.ts

--- src/types/panna-sdk.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "panna-sdk" {
-  export const PannaProvider: any;
-  export const LoginButton: any;
-  export const ConnectButton: any;
-  export const EmbeddedWallet: any;
-
-  const _default: any;
-  export default _default;
-}
